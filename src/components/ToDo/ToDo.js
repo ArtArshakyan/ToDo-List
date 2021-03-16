ae3f442ab754986bcfca017f1cfb1b8f16048129
Artur Arshakyan <arturworkspace5@gmail.com>
2021-03-17 00:26:45 +0400

    Created modal for new task, Added description, Created modal for editing task, Used Fontawesome, Added functional for selecting and deselecting all tasks

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -3,20 +3,24 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import Task from '../Task/Task';
 import NewTask from '../NewTask/NewTask';
 import Confirm from '../Confirm/Confirm';
+import EditTaskModal from '../EditTaskModal/EditTaskModal';
 import styles from './todo.module.css';
 
 class ToDo extends Component {
     state = {
         tasks: [],
         selectedTasks: new Set(),
-        showConfirm: false
+        showConfirm: false,
+        openNewTaskModal: false,
+        editTask: null
     };
 
     addTask = (newTask) => {
         const tasks = [...this.state.tasks, newTask];
 
         this.setState({
-            tasks
+            tasks,
+            openNewTaskModal: false
         });
     };
 
@@ -66,8 +70,42 @@ class ToDo extends Component {
         });
     };
 
+    selectAll = () => {
+        const taskIds = this.state.tasks.map((task) => task._id);
+        this.setState({
+            selectedTasks: new Set(taskIds)
+        });
+    };
+
+    deSelectAll = () => {
+        this.setState({
+            selectedTasks: new Set()
+        });
+    };
+
+    toggleNewTaskModal = () => {
+        this.setState({
+            openNewTaskModal: !this.state.openNewTaskModal
+        });
+    };
+
+    handleEdit = (editTask) => {
+        this.setState({ editTask });
+    };
+
+    handleSaveTask = (editedTask) => {
+        const tasks = [...this.state.tasks];
+        const foundIndex = tasks.findIndex((task) => task._id === editedTask._id);
+        tasks[foundIndex] = editedTask;
+
+        this.setState({
+            tasks,
+            editTask: null
+        });
+    };
+
     render() {
-        const { tasks, selectedTasks, showConfirm } = this.state;
+        const { tasks, selectedTasks, showConfirm, openNewTaskModal, editTask } = this.state;
 
         const taskComponents = tasks.map(task => {
             return (
@@ -77,13 +115,14 @@ class ToDo extends Component {
                     sm={6}
                     md={4}
                     lg={3}
-                    xl={2}
                 >
                     <Task
                         data={task}
                         onToggle={this.toggleTask}
                         disabled={!!selectedTasks.size}
                         onDelete={this.deleteTask}
+                        selected={selectedTasks.has(task._id)}
+                        onEdit={this.handleEdit}
                     />
                 </Col>
             );
@@ -96,22 +135,41 @@ class ToDo extends Component {
                         <Col
                             xs={12}
                         >
-                            <h2>ToDo List</h2>
-                        </Col>
-                        <Col
-                            xs={12}
-                        >
-                            <NewTask
-                                disabled={!!selectedTasks.size}
-                                onAdd={this.addTask}
-                            />
+                            <h2
+                                className="text-center"
+                            >
+                                ToDo List
+                            </h2>
                         </Col>
                     </Row>
                     <Row>
                         <Col
+                            className="text-center"
                             xs={12}
                         >
                             <Button
+                                className={styles.ToDoButtons}
+                                variant="success"
+                                onClick={this.toggleNewTaskModal}
+                            >
+                                Add New task
+                            </Button>
+                            <Button
+                                className={styles.ToDoButtons}
+                                variant="warning"
+                                onClick={this.selectAll}
+                            >
+                                Select All
+                            </Button>
+                            <Button
+                                className={styles.ToDoButtons}
+                                variant="warning"
+                                onClick={this.deSelectAll}
+                            >
+                                Deselect All
+                            </Button>
+                            <Button
+                                className={styles.ToDoButtons}
                                 variant="danger"
                                 onClick={this.toggleConfirm}
                                 disabled={!selectedTasks.size}
@@ -123,11 +181,27 @@ class ToDo extends Component {
                     <Row>{taskComponents}</Row>
                 </Container>
 
-                {showConfirm &&
+                {
+                    showConfirm &&
                     <Confirm
                         onClose={this.toggleConfirm}
                         onConfirm={this.removeSelected}
                         count={selectedTasks.size}
+                    />
+                }
+                {
+                    openNewTaskModal &&
+                    <NewTask
+                        onClose={this.toggleNewTaskModal}
+                        onAdd={this.addTask}
+                    />
+                }
+                {
+                    editTask &&
+                    <EditTaskModal
+                        data={editTask}
+                        onClose={() => this.handleEdit(null)}
+                        onSave={this.handleSaveTask}
                     />
                 }
             </div>
