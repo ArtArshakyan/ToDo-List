dfd01ed4a4dfb34bd3b015ac5840785e87ba8a57
Artur Arshakyan <arturworkspace5@gmail.com>
2021-03-25 15:57:58 +0400

    Added PureComponent in Task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import styles from './taskStyle.module.css';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
 
-class Task extends Component {
+class Task extends PureComponent {
 
     handleChange = () => {
         const { onToggle, data } = this.props;
